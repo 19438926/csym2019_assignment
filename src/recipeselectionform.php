3ca709fcc3fd9b60a345213a4a40570d52b599a5
19438926 <1204183324@qq.com>
2022-05-11 17:30:24 +0100

    task2

--- src/recipeselectionform.php
+++ src/recipeselectionform.php
@@ -26,13 +26,14 @@ $password = 'csym019';
 $schema = 'Recipes';
 $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password,
 [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$results = $pdo->query('SELECT * FROM recipe order by title');
-echo '<table><tr><th>TICK</th><th>title</th><th>preptime</th><th>cooktime</th><th>complexity</th><th>serves</th></tr>';
+$results = $pdo->query('select distinct(title) ,preptime ,cooktime,complexity,serves,kcal,fat,saturates,carbs,sugars,fibre,protein,salt from recipe natural join nutrition order by title');
+echo '<table><tr><th>TICK</th><th>title</th><th>preptime</th><th>cooktime</th><th>complexity</th><th>serves</th><th>kcal</th><th>fat</th><th>saturates</th><th>carbs</th><th>sugars</th><th>fibre</th><th>protein</th><th>salt</th></tr>';
   
 foreach ($results as $row) {
     
 
-    echo '<tr><td><input type="checkbox" /></td><td>'.$row['title'].'</td><td>'. $row['preptime'].'</td><td>'.$row['cooktime'].'</td><td>'.$row['complexity'].'</td><td>'.$row['serves'].'</td></tr>';
+    echo '<tr><td><input type="checkbox" /></td><td>'.$row['title'].'</td><td>'. $row['preptime'].'</td><td>'.$row['cooktime'].'</td><td>'.$row['complexity'].'</td><td>'.$row['serves'].'</td>
+    <td>'.$row['kcal'].'</td><td>'.$row['fat'].'</td><td>'.$row['saturates'].'</td><td>'.$row['carbs'].'</td><td>'.$row['sugars'].'</td><td>'.$row['fibre'].'</td><td>'.$row['protein'].'</td><td>'.$row['salt'].'</td></tr>';
 }
 echo '</table>';
 
